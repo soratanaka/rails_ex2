0c04122332ebbeb1413ef7b8ab5adcc21d7ef92e
sora tanaka <rari4423@gmail.com>
2022-09-28 11:58:21 +0900

    up

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,10 +9,14 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-    if @blog.save
-      redirect_to blogs_path, notice: "ブログを作成しました！"
-    else
+    if params[:back]
       render :new
+    else
+      if @blog.save
+        redirect_to blogs_path, notice: "ブログを作成しました！"
+      else
+        render :new
+      end
     end
   end
 
@@ -41,6 +45,7 @@ class BlogsController < ApplicationController
 
   def confirm
     @blog = Blog.new(blog_params)
+    render :new if @blog.invalid?
   end
 
   private
